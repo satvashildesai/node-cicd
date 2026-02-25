a50877860f61357a5029770ae5cbfa772e067936
satvashildesai <satvashildesai01@gmail.com>
2026-02-26 00:34:03 +0530

    Added comment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Application } from "express";
 
 const app: Application = express();
 
+// Health api.
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
